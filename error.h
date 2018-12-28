9dca0709da93ef6c80b34b8cfd4f99eed69d98d8
azaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2018-12-28 10:52:18 +0100

    Move include to top of file

--- error.h
+++ error.h
@@ -5,6 +5,8 @@
 #ifndef GAL4XY_ERROR_H
 #define GAL4XY_ERROR_H
 
+#include <stdio.h>
+
 #define MALLOC_ERROR(func, msg) fprintf(stderr, "%s: %s\n", (func), (msg))
 #define INSUFFICIENT_PLANETS_ERROR(func, planets, players) \
     fprintf(stderr, "%s: insufficient planets (%d) for all players (%d)\n", \
@@ -12,6 +14,5 @@
 #define INVALID_COLOR_ERROR(func, color) fprintf(stderr, "%s: invalid color (%s)\n", \
     (func), (color));
 
-#include <stdio.h>
 
 #endif //GAL4XY_ERROR_H
